6e9f35dd5b6cbfa2fb63096648c51804a2c2e136
truc <jasmine.trng@gmail.com>
2024-11-16 21:15:08 -0800

    react template

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Input, TextField } from '@mui/material';
+
+function App() {
+  return (
+    <div>
+      <TextField label="Outlined" variant="outlined" />
+      <Input></Input>
+
+    </div>
+  );
+}
+
+export default App;
